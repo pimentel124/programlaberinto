15db39f3bd465bf76943ec993d676a3723c357f9
Álvaro Pimentel <alvar@LAPTOP-UITBF30I>
2021-06-16 17:58:25 +0200

    Comentado Casillas

--- src/pkgfinal/Casillas.java
+++ src/pkgfinal/Casillas.java
@@ -11,113 +11,151 @@ public class Casillas {
 
     private int X;   //coordenada pixeles X
     private int Y;   //coordenada pixeles Y
-    private boolean ocupada;   //
-    public char[] limites;
-    private Rectangle2D.Float cuadrado;
-    private Puntero punt;
-    private int anchuralinea = 2;
+    
+    //booleano encargado de gestionar si la casilla está ocupada
+    private boolean ocupada;
+    
+     //booleano encargado de gestionar si la casilla es la de victoria
     private boolean win;
-    private Color cfondo = Color.WHITE;
-    private Color cpuntero = Color.RED;
-    private Color cparedes = Color.BLACK;
+    
+    //char[] con los la información sobre las paredes de la casilla
+    public char[] limites;  
+    
+    //cuadrado para gestionar la casilla
+    private Rectangle2D.Float cuadrado;
+    
+   
+    private Puntero punt;   //declaración del puntero
+    
+    private int anchuralinea = 2;  //se establece el ancho de las paredes
+    
+    
+    private Color cfondo = Color.WHITE;     //el color de fondo y su color por defecto
+    private Color cpuntero = Color.RED;     //el color del puntero y su color por defecto
+    private Color cparedes = Color.BLACK;   //el color de las paredes y su color por defecto
 
-    public Casillas() {
 
-        ocupada = false;
-        win = false;
-
-    }
-
+    //CONSTRUCTOR  
     public Casillas(Rectangle2D.Float c, char[] lim, int X, int Y) {
+        //por defecto todas las casillas NO estarán ocupadas
         ocupada = false;
+        
+        //se copian el resto de datos pasados al constructor a las variables privadas
         limites = lim;
         this.X = X;
         this.Y = Y;
         cuadrado = c;
     }
 
-
-    void setLibre() {
+    //METODOS SET encargados de establecer valores para los atributos de la clase
+    
+    public void setOcupada() { //ocupa la casilla
+        ocupada = true;
+    }
+    
+    void setLibre() {       //libera la casilla
+        
         ocupada = false;
     }
+     
+    public void setWin() {   //establece la casilla como ganadora
+        win = true;
+    }
 
-    public void setColorFondo(Color fondo) {
+    public void setColorFondo(Color fondo) {  //establece el color de fondo
         this.cfondo = fondo;
 
     }
 
-    public void setColorPuntero(Color puntero) {
+    public void setColorPuntero(Color puntero) { //establece el color del puntero
         this.cpuntero = puntero;
     }
 
-    public void setColorParedes(Color paredes) {
+    public void setColorParedes(Color paredes) { //establece el color de las paredes
         this.cparedes = paredes;
     }
 
-    public void setWin() {
-        win = true;
-    }
-
-    boolean info() {
+    boolean info() {  //devuelve true si la casilla está ocupada y false de lo contrario
         return ocupada;
 
     }
 
-    public void setOcupada() {
-        ocupada = true;
-    }
-
-    public char[] getLimites() {
+    public char[] getLimites() { //devuelve el char[] con la información de las paredes
         return limites;
     }
 
+    //Método encargado de pintar la casilla
     public void paintComponent(Graphics g) throws Exception {
 
-        //System.out.println(limites);
-        Graphics2D aux = (Graphics2D) g;
-        Graphics2D puntero = (Graphics2D) g;
-
+        //Se genera un Graphics2D a partir del Graphics pasado por parámetro desde Laberinto 
+        
+        Graphics2D aux = (Graphics2D) g;     
+        
+        //se le asigna el color de fondo
         aux.setColor(cfondo);
+        //se dibuja el cuadrado antes pasado al costructor
         aux.fill(cuadrado);
-
-        if (limites[0] == '1') {   //NORTE
+        
+        //DIBUJADO DE PAREDES
+        //el lugar de dibujar líneas, se dibujan rectánculos muy finos paraque, en caso de querer,
+        //se pueda modificar el grosor de las líneas
+        
+        if (limites[0] == '1') {   // PARED NORTE == 1
+            
+            //se dibuja un rectángulo que empieza en las coordenadas (X , Y) 
+            //y tiene como anchura el lado de la casilla y como altura el grosor de línea
             Rectangle2D.Float pared = new Rectangle2D.Float(X, Y, Laberinto.getDimension(), anchuralinea);
 
+            //se asigna el color de las paredes
             aux.setColor(cparedes);
+            //se dibuja la pared
             aux.fill(pared);
-            //System.out.println("norte");
 
         }
 
-        if (limites[1] == '1') {  //ESTE //DERECHA
+        if (limites[1] == '1') {  //PARED ESTE == 1
+            
+            //se dibuja un rectángulo que empieza en las coordenadas (X+ladocasilla , Y) 
+            //y tiene como anchura el el grosor de la línea y como altura ladocasilla
             Rectangle2D.Float pared = new Rectangle2D.Float(X + Laberinto.getDimension(), Y, anchuralinea, Laberinto.getDimension());
+            
+            //se asigna el color de las paredes
             aux.setColor(cparedes);
+            //se dibuja la pared
             aux.fill(pared);
-            //System.out.println("derecha");
+            
 
         }
 
-        if (limites[2] == '1') {   //SUR   
-//           aux.setColor(Color.RED);
-//           aux.drawLine(X, Y+laberinto.getDimension(), X+laberinto.getDimension(), Y+laberinto.getDimension());
-//           aux.setColor(Color.BLACK);
-//           aux.drawLine(X, Y, X+laberinto.getDimension(), Y+laberinto.getDimension());
+        if (limites[2] == '1') {   //PARED SUR == 1
+            
+            //se dibuja un rectángulo que empieza en las coordenadas (X , Y+ladocasilla) 
+            //y tiene como anchura el lado de la casilla y como altura el grosor de línea
             Rectangle2D.Float pared = new Rectangle2D.Float(X, Y + Laberinto.getDimension(), Laberinto.getDimension(), anchuralinea);
+            
+            //se asigna el color de las paredes
             aux.setColor(cparedes);
+            //se dibuja la pared
             aux.fill(pared);
-            //System.out.println("sur");
 
         }
 
-        if (limites[3] == '1') {   //OESTE   //IZQUIERDA
+        if (limites[3] == '1') {   //PARED OESTE == 1 
+            
+            //se dibuja un rectángulo que empieza en las coordenadas (X , Y) 
+            //y tiene como anchura el lado de la casilla y como altura el grosor de línea
             Rectangle2D.Float pared = new Rectangle2D.Float(X, Y, anchuralinea, Laberinto.getDimension());
+            
+            //se asigna el color de las paredes
             aux.setColor(cparedes);
+            //se dibuja la pared
             aux.fill(pared);
-            //System.out.println("izquierda");
 
         }
+        
+        //en el caso de que la casilla sea la de victoria 
         if (this.win) {
-            Rectangle2D.Float cwin = new Rectangle2D.Float(X + 2, Y + 2, Laberinto.getDimension() - 2, Laberinto.getDimension() - 2);
+            Rectangle2D.Float cwin = new Rectangle2D.Float(X + anchuralinea, Y + anchuralinea, Laberinto.getDimension(), Laberinto.getDimension());
             aux.setColor(Color.lightGray);
             aux.fill(cwin);
         }
